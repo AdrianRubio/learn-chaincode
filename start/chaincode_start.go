72ae3f39bba3876295d3cd2d9fa605ee2cf3c59a
arubio <adrian.rubio@mgt-consulting.com>
2016-11-24 15:37:55 +0100

    Compiled my code

--- start/chaincode_start.go
+++ start/chaincode_start.go
@@ -66,7 +66,7 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
     return nil, errors.New("Received unknown function invocation: " + function)
 }
 
-func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
     var key, value string
     var err error
     fmt.Println("running write()")
@@ -97,7 +97,7 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
     return nil, errors.New("Received unknown function query: " + function)
 }
 
-func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
     var key, jsonResp string
     var err error
 
